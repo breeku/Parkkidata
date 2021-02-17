fa5d3feb18ab55109dd912ed10883d0137cd821a
breeku <bregfi@hotmail.com>
2021-02-17 14:29:08 +0200

    parking_area seed

--- backend/src/database/seeds/01-parking_area.js
+++ backend/src/database/seeds/01-parking_area.js
@@ -4,10 +4,30 @@ exports.seed = function (knex) {
     // Deletes ALL existing entries
     return knex("parking_area")
         .del()
-        .then(function () {
+        .then(async function () {
             // Inserts seed entries
             // hae data axios.get() parkkihubista
             // tallenna data tietokantaan
             // hae lisää dataa parkkihubista, "next" on seuraava url
+            let next = "https://pubapi.parkkiopas.fi/public/v1/parking_area/"
+
+            while (next) {
+                try {
+                    const { data } = await axios.get(next)
+                    for (const feature of data.features) {
+                        await knex("parking_area").insert({
+                            uid: feature.id,
+                            geometry: JSON.stringify(feature.geometry),
+                            capacity_estimate:
+                                feature.properties.capacity_estimate,
+                        })
+                    }
+                    next = data.next
+                    console.log(next)
+                } catch (e) {
+                    console.error(e)
+                }
+            }
+            return
         })
 }
