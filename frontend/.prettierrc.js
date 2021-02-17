2b4e8b44e1869d58c44a02e93c4eed3162f08147
breeku <bregfi@hotmail.com>
2021-02-17 16:49:28 +0200

    add prettier configs

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: false,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    printWidth: 90,
+    endOfLine: 'crlf',
+}
