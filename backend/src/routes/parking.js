388c0e73208390199caa51f194073f376efc7c4b
breeku <bregfi@hotmail.com>
2021-02-18 18:35:12 +0200

    add swagger with redoc

--- backend/src/routes/parking.js
+++ backend/src/routes/parking.js
@@ -1,24 +1,71 @@
-const express = require("express")
-const axios = require("axios")
+const express = require('express')
+const axios = require('axios')
 const router = express.Router()
-const { getParkingAreas } = require("../database/queries/parking_area")
+const { getParkingAreas } = require('../database/queries/parking_area')
 
-const BASEURL = "https://pubapi.parkkiopas.fi/public/v1"
+const BASEURL = 'https://pubapi.parkkiopas.fi/public/v1'
 
-router.get("/parking_area/", async (req, res) => {
+/**
+ * @swagger
+ * /api/parking_area/:
+ *   get:
+ *     summary: Get a list of parking areas
+ *     description: Fetch info about parking areas as GeoJSON feature collection.
+ *     responses:
+ *       200:
+ *         description: An array of parking areas with metadata information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Parking_Area'
+ *
+ */
+router.get('/parking_area/', async (req, res) => {
     try {
         const data = await getParkingAreas()
         res.send(data)
     } catch (e) {
         console.error(e)
+        res.sendStatus(500)
     }
 })
 
-router.get("/parking_area/page/:page", async (req, res) => {
+router.get('/parking_area/page/:page', async (req, res) => {
+    const page = req.params.page
+    try {
+        const { data } = await axios.get(BASEURL + '/parking_area/?page=' + page)
+        res.send(data)
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+router.get('/parking_area/id/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const { data } = await axios.get(BASEURL + '/parking_area/' + id)
+        res.send(data)
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+router.get('/parking_area_statistics', async (req, res) => {
+    try {
+        const { data } = await axios.get(BASEURL + '/parking_area_statistics/')
+        res.send(data)
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+router.get('/parking_area_statistics/page/:page', async (req, res) => {
     const page = req.params.page
     try {
         const { data } = await axios.get(
-            BASEURL + "/parking_area/?page=" + page
+            BASEURL + '/parking_area_statistics/?page=' + page,
         )
         res.send(data)
     } catch (e) {
@@ -26,43 +73,10 @@ router.get("/parking_area/page/:page", async (req, res) => {
     }
 })
 
-router.get("/parking_area/id/:id", async (req, res) => {
+router.get('/parking_area_statistics/id/:id', async (req, res) => {
     const id = req.params.id
     try {
-        const { data } = await axios.get(BASEURL + "/parking_area/" + id)
-        res.send(data)
-    } catch (e) {
-        console.error(e)
-    }
-})
-
-router.get("/parking_area_statistics", async (req, res) => {
-    try {
-        const { data } = await axios.get(BASEURL + "/parking_area_statistics/")
-        res.send(data)
-    } catch (e) {
-        console.error(e)
-    }
-})
-
-router.get("/parking_area_statistics/page/:page", async (req, res) => {
-    const page = req.params.page
-    try {
-        const { data } = await axios.get(
-            BASEURL + "/parking_area_statistics/?page=" + page
-        )
-        res.send(data)
-    } catch (e) {
-        console.error(e)
-    }
-})
-
-router.get("/parking_area_statistics/id/:id", async (req, res) => {
-    const id = req.params.id
-    try {
-        const { data } = await axios.get(
-            BASEURL + "/parking_area_statistics/" + id
-        )
+        const { data } = await axios.get(BASEURL + '/parking_area_statistics/' + id)
         res.send(data)
     } catch (e) {
         console.error(e)
---
+++ backend/src/swagger/swaggerDefinition.js
@@ -0,0 +1,14 @@
+module.exports = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API',
+        version: '1.0.0',
+        description: 'Lorem ipsum',
+    },
+    servers: [
+        {
+            url: 'http://localhost:3001',
+            description: 'Development server',
+        },
+    ],
+}
