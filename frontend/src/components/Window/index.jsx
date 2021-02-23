bc601e1b789401679efa8a7918c4a3d584bda736
breeku <bregfi@hotmail.com>
2021-02-23 19:00:34 +0200

    fix #11

--- frontend/src/components/Window/index.jsx
+++ frontend/src/components/Window/index.jsx
@@ -8,7 +8,7 @@ import { getParkingStatistics } from '../../services/parking'
 
 import { getParkingHistory } from '../../services/parking'
 
-import { LineChart, Line ,CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 
 export default function Window() {
     const [parkingStatistics, setParkingStatistics] = useState(null)
@@ -30,19 +30,22 @@ export default function Window() {
     const CustomTooltip = ({ active, payload, label }) => {
         if (active && payload && payload.length) {
             const date = new Date(label)
-            const labelDate = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear()
-            label = date.getUTCHours()+2 + ".00"
-          return (
-            <div style={{backgroundColor: "white", opacity: "90%",}} className="custom-tooltip">
-              <p className="label">{`date: ${labelDate}`}</p>
-              <p className="label">{`time: ${label}`}</p>
-              <p className="label">{`parking count: ${payload[0].value}`}</p>
-            </div>
-          );
+            const labelDate =
+                date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear()
+            label = date.getUTCHours() + 2 + '.00'
+            return (
+                <div
+                    style={{ backgroundColor: 'white', opacity: '90%' }}
+                    className='custom-tooltip'>
+                    <p className='label'>{`date: ${labelDate}`}</p>
+                    <p className='label'>{`time: ${label}`}</p>
+                    <p className='label'>{`parking count: ${payload[0].value}`}</p>
+                </div>
+            )
         }
-      
-        return null;
-      };
+
+        return null
+    }
 
     useEffect(() => {
         ;(async () => {
@@ -99,12 +102,13 @@ export default function Window() {
             defaultPosition={{ x: 0, y: 0 }}
             position={null}
             scale={1}
-            handle='.handle'>
+            cancel='.disable'>
             <div
                 style={{
                     display: 'inline-flex',
                     flexDirection: 'column',
                     alignItems: 'center',
+                    position: 'absolute',
                 }}>
                 <div
                     style={{
@@ -119,7 +123,7 @@ export default function Window() {
                     <br />
                     Parking capacity (estimate):
                     <br />
-                    <div>
+                    <div className='disable'>
                         <select onChange={e => setOperator(e.target.value)}>
                             <option value='more than'>More than</option>
                             <option value='less than'>Less than</option>
@@ -151,33 +155,44 @@ export default function Window() {
                         width: '200px',
                         padding: 10,
                         textAlign: 'center',
-                    }}
-                    className='handle'>
+                    }}>
                     uid: {selected?.uid}
                     <br />
                     capacity estimate: {selected?.capacity_estimate || 'unknown'}
                     <br />
                     current parking count: {parkingStatistics}
-                    </div>
-                
-                {parkingHistory &&
-                <div 
-                    style={{
-                        backgroundColor: "white",
-                        display: "inline-block",
-                        width: "400px",
-                        height: "400px",
-                        marginTop: 3,
-                    }}>
-                     <LineChart width={400} height={400} data={parkingHistory}>
-                        <Line type="monotone" dataKey="current_parking_count" stroke="#8884d8" />
-                        <CartesianGrid stroke="#ccc" strokeDasharray="1 1" />
-                        <XAxis dataKey="created_at"  tickFormatter={(formtime) => new Date(formtime).toLocaleTimeString(["en-US"], {hour: '2-digit'})}/>
-                        <YAxis dataKey="current_parking_count"/>
-                        <Tooltip content={CustomTooltip}/>
-                    </LineChart>
-                </div>}
                 </div>
+
+                {parkingHistory && (
+                    <div
+                        style={{
+                            backgroundColor: 'white',
+                            display: 'inline-block',
+                            width: '400px',
+                            height: '400px',
+                            marginTop: 3,
+                        }}>
+                        <LineChart width={400} height={400} data={parkingHistory}>
+                            <Line
+                                type='monotone'
+                                dataKey='current_parking_count'
+                                stroke='#8884d8'
+                            />
+                            <CartesianGrid stroke='#ccc' strokeDasharray='1 1' />
+                            <XAxis
+                                dataKey='created_at'
+                                tickFormatter={formtime =>
+                                    new Date(formtime).toLocaleTimeString(['en-US'], {
+                                        hour: '2-digit',
+                                    })
+                                }
+                            />
+                            <YAxis dataKey='current_parking_count' />
+                            <Tooltip content={CustomTooltip} />
+                        </LineChart>
+                    </div>
+                )}
+            </div>
         </Draggable>
     )
 }
